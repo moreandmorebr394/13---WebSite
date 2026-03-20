4b01bcda955d3dba6bb8a40cae99881c2ceb407d
Bruna A. <130995251+moreandmorebr394@users.noreply.github.com>
2026-03-20 11:50:07 -0300

    Update 3 - Três Framework.py

--- 3 - Três Framework.py
+++ 3 - Três Framework.py
@@ -1,28 +1,34 @@
-
 import tkinter as tk
 
 root = tk.Tk()
 root.geometry("1400x700")
 root.title("Estrutura Base")
 
-frame_inferior_direito = tk.Frame(root, height=700, width=1400, bg="yellow")
-frame_inferior_direito.place (relx=0, rely=0, relwidth= 50,relheight=100)
-#frame_inferior_direito.pack(fill=tk.Y, side=tk.RIGHT)
-label_inferior_direito = tk.Label(frame_inferior_direito, text="Frame Inferior Direito", bg="yellow")
-label_inferior_direito.place(anchor="se")
+#Telas com cores diversas de proporções diferentes
 
+frame_pagina = tk.Frame(root, bg="tomato")
+frame_pagina.place (relx=0.1, rely=0, relwidth= 0.8,relheight=0.05)
 
-frame_direito = tk.Frame(root, height=500, width=1400, bg="lightgreen")
-frame_direito.place (relx=0, rely=0,relwidth= 200,relheight=0.70)
-#frame_direito.pack(fill=tk.X, side=tk.BOTTOM)
-label_direito = tk.Label(frame_direito, text="Frame Direito",font=("Roboto", 12), bg="lightgreen")
-label_direito.place(anchor="e")
+frame_first = tk.Frame(root, bg="lightsalmon")
+frame_first.place (relx=0.1, rely=0.11, relwidth= 0.8,relheight=0.2)
 
-frame_esquerdo = tk.Frame(root, height=700, width=200, bg="lightblue")
-frame_esquerdo.place (relx=0, rely=0, relwidth= 0.2,relheight=100)
-#frame_esquerdo.pack(side=tk.TOP)
-label_esquerdo = tk.Label(frame_esquerdo, text="Frame Esquerdo", bg="lightblue")
-label_esquerdo.place(anchor="nw")
+frame_second = tk.Frame(root, bg="maroon")
+frame_second.place (relx=0.1, rely=0.32, relwidth= 0.8,relheight=0.4)
 
+frame_third = tk.Frame(root, bg="palevioletred")
+frame_third.place (relx=0.1, rely=0.73, relwidth= 0.8,relheight=0.2)
 
+#Nome de cada tela
+
+label_pagina = tk.Label(frame_pagina, text="Páginas de Código",font=("Impact", 12), bg="tomato")
+label_pagina.place(relx= 0.5, rely= 0.4,anchor="center")
+
+label_first = tk.Label(frame_first, text="Primeiro Frame",font=("Roboto", 12), bg="lightsalmon")
+label_first.place(relx= 0.5, rely= 0.4,anchor="center")
+
+label_second = tk.Label(frame_second, text="Segundo Frame",font=("Roboto", 12), bg="maroon")
+label_second.place(relx= 0.5, rely= 0.4,anchor="center")
+
+label_third = tk.Label(frame_third, text="Terceiro Frame",font=("Roboto", 12), bg="palevioletred")
+label_third.place(relx= 0.5, rely= 0.4,anchor="center")
 root.mainloop()
